d839b8e2db5786199dd149e139df62f8f66f8a48
Anya L Keller <kelleranyal@gmail.com>
2019-07-28 13:17:02 -0400

    got game confirm working

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,17 @@
-function start_game() {
-    this.wins = 0;
-    this.losses = 0;
-    this.game_num = 0;
+class Game {
+    constructor() {
+        this.wins = 0;
+        this.losses = 0;
+        this.game_num = 0;
+        this.playing = true;
+    }
+}
 
-    this.confirm_begin = function(version) {
-        $("#modalthing").modal("show");
-        return;
-    };
-
-    this.play = function(version) {
+class tos extends Game {
+    constructor() {
+        super();
+    }
+    play() {
         this.game_num++;
-        console.log(version);
-        return;
-    };
-    console.log("game started");
+    }
 }
